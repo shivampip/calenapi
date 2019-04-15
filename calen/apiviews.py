a06f805fb4e06ee2d1f33a32f9c28c2ad2158792
Shivam Agrawal <Shivam.Agrawal@lntinfotech.com>
2019-04-15 11:52:32 +0530

    updated

--- calen/apiviews.py
+++ calen/apiviews.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status 
+from rest_framework import generics
 
 from .models import Person, Event
 from .serializers import PersonSerializer, EventSerializer
@@ -29,6 +30,10 @@ class CreatePerson(APIView):
         else:
             return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
 
+
+class CreateEventGen(generics.ListCreateAPIView):
+    queryset= Event.objects.all()
+    serializer_class= EventSerializer
 
 class CreateEvent(APIView):
     
--- calen/urls.py
+++ calen/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .apiviews import CreatePerson, CreateEvent
+from .apiviews import CreatePerson, CreateEvent, CreateEventGen
 
 urlpatterns = [
     path('makeperson/', CreatePerson.as_view(), name= 'make_person'),
     path('makeevent/', CreateEvent.as_view(), name= 'make_event'),
+    path('makeeventgen/', CreateEventGen.as_view(), name= 'make_event_gen'),
 ]
