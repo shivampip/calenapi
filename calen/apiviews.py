d5c59b5bd0b1dbb2a373c0a8692fe4f4fcb0ad18
Shivam Agrawal <Shivam.Agrawal@lntinfotech.com>
2019-04-19 14:31:19 +0530

    updated

--- calen/apiviews.py
+++ calen/apiviews.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status 
 from rest_framework import generics
 
+from django.contrib.auth import authenticate
+
 from .models import Person, Event
 from .serializers import PersonSerializer, EventSerializer, UserSerializer
 
@@ -87,3 +89,17 @@ class CreateUser(generics.CreateAPIView):
     authentication_classes= ()
     permission_classes= ()
     serializer_class= UserSerializer
+
+
+
+class LoginView(APIView):
+    permission_classes= ()
+
+    def post(self, request):
+        username= request.data.get("username")
+        password= request.data.get("password")
+        user= authenticate(username= username, password= password)
+        if(user):
+            return Response({"token": user.auth_token.key}, status= status.HTTP_200_OK)
+        else:
+            return Response({"error": "Wrong credentials"}, status= status.HTTP_400_BAD_REQUEST)
--- calen/urls.py
+++ calen/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .apiviews import CreatePerson, CreateEvent, CreateEventGen, CreateUser
+from .apiviews import CreatePerson, CreateEvent, CreateEventGen, CreateUser, LoginView
+
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('makeperson/', CreatePerson.as_view(), name= 'make_person'),
     path('makeevent/', CreateEvent.as_view(), name= 'make_event'),
     path('makeeventgen/', CreateEventGen.as_view(), name= 'make_event_gen'),
     path('users/', CreateUser.as_view(), name= 'make_user'),
+    path('login/', LoginView.as_view(), name= "login_view"),
+    #path('llogin/', obtain_auth_token, name= "llogin_view"),
 ]
