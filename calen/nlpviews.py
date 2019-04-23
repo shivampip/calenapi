c31e12f30cc1f2afd99c3e4573365b28e1a8b031
shivam301296 <shivam301296@gmail.com>
2019-04-23 22:51:18 +0530

    duration

--- calen/nlpviews.py
+++ calen/nlpviews.py
@@ -11,11 +11,48 @@ from .serializers import EventSerializer, UserSerializer, PendingEventSerializer
 
 import json 
 
+from duckling import DucklingWrapper
+
+from datetime import timedelta
+
 from hinlp.bot import MyBot
+print("Importing NLU model....", end= "")
 mb= MyBot()
 mb.initNlu()
+print("DONE")
+print("Importing Duckling.....", end= "")
+dw= DucklingWrapper()
+print("DONE")
 
 class Talk(APIView):
+
+
+    def get_duration(self, data):
+        out= ""
+        total= 0
+        for dd in data:
+            out+= "Dimention: "+dd['dim']+"<br>"
+            out+= "Text: "+dd['text']+"<br>"
+            val= dd['value']
+            tt= 0
+            if(val['second'] is not None):
+                tt+= timedelta(seconds= val['second']).total_seconds()
+            elif(val['minute'] is not None):
+                tt+= timedelta(minutes= val['minute']).total_seconds()
+            elif(val['hour'] is not None):
+                tt+= timedelta(hours= val['hour']).total_seconds()
+            elif(val['day'] is not None):
+                tt+= timedelta(days= val['day']).total_seconds()
+            elif(val['month'] is not None):
+                tt+= timedelta(months= val['month']).total_seconds()
+            elif(val['year'] is not None):
+                tt+= timedelta(years= val['year']).total_seconds()
+            
+            out+= "Duration in seconds: "+str(tt)+"<br>"
+            total+= tt 
+        out+= "<b>Total duration in seconds: "+str(total)+"</b><br>"
+        return out 
+
     def get(self, request):
         msg= request.GET.get('msg', None)
         if(msg is not None):
@@ -30,6 +67,16 @@ class Talk(APIView):
                 if(name in fields):
                     fields.remove(name)
             res+= "Remaining fields: "+str(fields)
+
+            res+="<br><br>Time<br>"
+            res+= str(dw.parse_time(msg))
+
+            res+="<br><br>Duration<br>"
+            duration= dw.parse_duration(msg)
+            res+= str(duration)
+
+            res+= "<br><br>"+str(self.get_duration(duration))
+
             return HttpResponse(res, status= status.HTTP_200_OK)
         person= request.GET.get('person', None)
         if(person is not None):
